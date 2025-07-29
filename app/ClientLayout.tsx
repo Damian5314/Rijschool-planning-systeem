4b60317bf357b770b59b3a178d7f120b9f29cb24
Wishant010 <144028795+Wishant010@users.noreply.github.com>
2025-07-29 23:39:43 +0200

    client layout

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -3,7 +3,9 @@
 import type React from "react"
 
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "sonner"
 import { AuthGuard } from "@/components/auth-guard"
+import { AuthProvider } from "@/contexts/auth-context"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/sidebar"
 
@@ -13,10 +15,13 @@ export default function ClientLayout({
   children: React.ReactNode
 }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <AuthGuard>{children}</AuthGuard>
-      <Toaster />
-    </SidebarProvider>
+    <AuthProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <AuthGuard>{children}</AuthGuard>
+        <Toaster />
+        <SonnerToaster richColors position="top-right" />
+      </SidebarProvider>
+    </AuthProvider>
   )
 }
